fdd7862c2e40489d60689c459486c33786364b47
nsb199 <singhneeraj199199@gmail.com>
2024-05-01 21:42:17 +0530

    changed the api

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const jokeBtn = document.getElementById('jokeBtn');
 const jokeContainer = document.getElementById('jokeContainer');
 const loading = document.getElementById('loading');
+const API_KEY = 'IMBUzQwWi0pDazq8S0fatQ==prJoIZvlpAJj9qWa';
 
 jokeBtn.addEventListener('click', fetchJoke);
 
 async function fetchJoke() {
   try {
-    loading.style.display = 'block'; // Show loading message
-    jokeContainer.innerText = ''; // Clear previous joke
-    const response = await fetch('https://icanhazdadjoke.com/', {
+    loading.style.display = 'block'; 
+    jokeContainer.innerText = ''; 
+    const response = await fetch('https://api.api-ninjas.com/v1/dadjokes', {
       headers: {
-        'Accept': 'application/json'
-      }
+        'X-Api-Key': API_KEY,
+      },
     });
     if (!response.ok) {
       throw new Error('Failed to fetch joke. Please try again.');
     }
     const data = await response.json();
-    displayJoke(data.joke);
+    displayJoke(data[0].joke);
   } catch (error) {
     console.error('Error fetching joke:', error);
     jokeContainer.innerHTML = `<div class="error">${error.message} <span class="smiley">&#128514;</span></div>`;
   } finally {
-    loading.style.display = 'none'; // Hide loading message
+    loading.style.display = 'none'; 
   }
 }
 
